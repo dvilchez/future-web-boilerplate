c30a4feb583cb5e8a43d5963da8c0bf8f1136fb9
David <dvilchez@xuaps.com>
2015-11-02 08:58:49 +0100

    Add react component render by server
    Why:
    
    We need a universal js app
    
    This change addresses the need by:
    
    Add react component
    Configure react router
    Configure express routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var hello = require('./hello')
+import express from 'express';
+import { Route, match, RoutingContext } from 'react-router';
+import hello from './hello';
+import { renderToString } from 'react-dom/server';
+import React from 'react';
+import Router from 'react-router';
+import Hello from 'hello-cmp';
 
-var router = express.Router();
+const router = express.Router();
+const routes = (<Route component={ Hello } path="/" />);
 
 router.use('/api/hello', hello);
+router.get('/', function (req, res) {
+    match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+        if (error) {
+          res.status(500).send(error.message)
+        } else if (redirectLocation) {
+          res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+        } else if (renderProps) {
+          res.status(200).render('index', {content: renderToString(<RoutingContext {...renderProps} />)})
+        } else {
+          res.status(404).send('Not found')
+        }
+  });
+});
 
-module.exports = router;
+export default router;
